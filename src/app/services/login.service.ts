e50d7feda39bc167f42baedf8471af9974abc65f
Matias Criollo <matiascriollo57@gmail.com>
2024-02-03 12:51:36 -0500

    Crear varios métodos en el sevicio de login

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -9,8 +9,52 @@ export class LoginService {
 
   constructor(private httpClient: HttpClient) { }
 
-  //Generar token
+  // Generar token
   public generateToken(loginData: any) {
     return this.httpClient.post(`${baserUrl}/generate-token`, loginData);
+  }
+
+  // Iniciamos sesión y establacemos el token en el localStorage
+  public loginUser(token: any) {
+    localStorage.setItem('token', token);
+  }
+
+  public isLoggedIn() {
+    let tokenStr = localStorage.getItem('token');
+    if (tokenStr == undefined || tokenStr == '' || tokenStr == null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  // Cerramos sesión y eliminamos el token
+  public logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    return true;
+  }
+
+  // Obtenemos el token
+  public getToken() {
+    return localStorage.getItem('token');
+  }
+
+  public setUser(user: any) {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  public getUser() {
+    let userStr = localStorage.getItem('user');
+    if (userStr != null) {
+      return JSON.parse(userStr);
+    } else {
+      this.logout;
+    }
+  }
+
+  public getUserRole() {
+    let user = this.getUser();
+    return user.authorities[0].authority;
   }
 }
