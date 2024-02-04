ac495196ff0c51ae450f488546fa1959a268c504
Matias Criollo <matiascriollo57@gmail.com>
2024-02-04 12:46:57 -0500

    Creación del welcome component y diseñando el perfil de usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,15 +8,17 @@ import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboa
 import { NormalGuard } from './services/normal.guard';
 import { AdminGuard } from './services/admin.guard';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { WelcomeComponent } from './pages/admin/welcome/welcome.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'signup', component: SignupComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
 
-  { path: 'admin', component: DashboardComponent, pathMatch: 'full', canActivate: [AdminGuard], children: [{
-    path: 'profile', component: ProfileComponent
-  }] },
+  { path: 'admin', component: DashboardComponent, canActivate: [AdminGuard], children: [
+    { path: 'profile', component: ProfileComponent },
+    { path: '', component: WelcomeComponent }
+  ] },
   
   { path: 'user-dashboard', component: UserDashboardComponent, pathMatch: 'full', canActivate: [NormalGuard] }
 ];
