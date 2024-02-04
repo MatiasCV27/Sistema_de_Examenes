7524a9d77678e1c3b871f74ecd12eb609c91d37d
Matias Criollo <matiascriollo57@gmail.com>
2024-02-03 23:19:44 -0500

    Implementando la función del login y logout

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { LoginService } from 'src/app/services/login.service';
 
 @Component({
@@ -14,7 +15,7 @@ export class LoginComponent implements OnInit {
       "password": ''
   }
 
-  constructor(private snack: MatSnackBar, private loginService: LoginService) { }
+  constructor(private snack: MatSnackBar, private loginService: LoginService, private router: Router) { }
 
   ngOnInit(): void {
       
@@ -46,10 +47,12 @@ export class LoginComponent implements OnInit {
 
           if (this.loginService.getUserRole() == "ADMIN") {
             // Dashboard admin
-            window.location.href = '/admin';
+            //window.location.href = '/admin';
+            this.router.navigate(['admin']);
           } else if (this.loginService.getUserRole() == "NORMAL") {
             // User dashnoard
-            window.location.href = '/user-dashboard';
+            //window.location.href = '/user-dashboard';
+            this.router.navigate(['user-dashboard']);
           } else {
             this.loginService.logout();
           }
