5ae935eaf67103892d7c65e00b3e5ea7bc6666a0
Matias Criollo <matiascriollo57@gmail.com>
2024-02-03 19:09:42 -0500

    Agregando los guards para admin y usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { SignupComponent } from './pages/signup/signup.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
 import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboard.component';
+import { NormalGuard } from './services/normal.guard';
+import { AdminGuard } from './services/admin.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'signup', component: SignupComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
-  { path: 'admin', component: DashboardComponent, pathMatch: 'full' },
-  { path: 'user-dashboard', component: UserDashboardComponent, pathMatch: 'full' },
+  { path: 'admin', component: DashboardComponent, pathMatch: 'full', canActivate: [AdminGuard] },
+  { path: 'user-dashboard', component: UserDashboardComponent, pathMatch: 'full', canActivate: [NormalGuard] },
 ];
 
 @NgModule({
